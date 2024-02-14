f4e3d22e55af083c50daf73744309a053234ab62
Harshalchute <harshalchute1@gmail.com>
2024-02-14 15:49:11 +0530

    Encapsulation in Java Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+//        rwOnly.name = "XYZ";
+//        System.out.print(name);
+//      name has private access in com.driver.RWOnly
+
+        rwOnly.setName("ABC");
+        System.out.println(rwOnly.getName());
+    }
 }
